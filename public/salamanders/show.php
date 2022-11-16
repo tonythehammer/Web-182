c9c23b0fbadfa259f7090cb11e99c451e8f8e7a7
Anthony Harwood <anthonywharwood@students.abtech.edu>
2022-11-16 10:03:34 -0500

    updated code

--- public/salamanders/show.php
+++ public/salamanders/show.php
@@ -1,14 +1,17 @@
-<?php require_once('../../private/initialize.php');
+<?php require_once('../../private/initialize.php'); 
 
-$id = $_GET['id'] ?? '1'; // PHP > 7.0
-$page_title = 'View Salamander';
+$page_title = 'Salamander Details';
 include(SHARED_PATH . '/salamander-header.php'); 
-
+$id = $_GET['id'] ?? '1'; 
+$salamander = find_salamander_by_id($id);
 ?>
 
+<h2>Salamander Details</h2>
+<p><strong>ID:</strong><?= h($salamander['id']); ?></p>
+<p><strong>Name:</strong><?= h($salamander['name']); ?></p>
+<p><strong>Habitat:</strong><br> <?= h($salamander['habitat']); ?> </p>
+<p><strong>Description:</strong><br> <?= h($salamander['description']); ?></p>
 
-  <a href="<?= url_for('/salamanders/index.php'); ?>">&laquo; Back to List</a>
-
-   <p> Page ID: <?= h($id); ?> </p>
+<p><a href="<?= url_for('/salamanders/index.php'); ?>">&laquo; Back to Salamander List</a></p>
 
 <?php include(SHARED_PATH . '/salamander-footer.php'); ?>
