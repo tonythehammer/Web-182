20c0e29a00e10685638dde644d8ded3b9636c1fe
Anthony Harwood <anthonywharwood@students.abtech.edu>
2022-11-16 11:31:27 -0500

    final

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,5 +1,12 @@
 <?php
-define("DB_SERVER", "");
-define("DB_USER", "");
-define("DB_PASS", "");
-define("DB_NAME", "");
+// define("DB_SERVER", "");
+// define("DB_USER", "");
+// define("DB_PASS", "");
+// define("DB_NAME", "");
+
+
+// local
+define("DB_SERVER", "localhost");
+define("DB_USER", "tony");
+define("DB_PASS", "Awh28806AWH!");
+define("DB_NAME", "sas");
